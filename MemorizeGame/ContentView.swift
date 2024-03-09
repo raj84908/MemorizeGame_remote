746512623a0928bf709847193c01ef9465a92405
Raj Patel <raj84908@gmail.com>
2024-03-09 15:37:23 -0500

    Initial Commit

---
+++ MemorizeGame/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by Raj Patel on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            HStack {
+                CardView(isFaceUp: true)
+                CardView()
+                CardView()
+                CardView()
+            }
+            
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+ }
+}
+ 
---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Raj Patel on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
